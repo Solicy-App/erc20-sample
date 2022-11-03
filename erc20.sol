e664e92b4ad6339984206401cdbe7c97cb26db52
Edgar Ohanyan <edgarohanyan1994@gmail.com>
2022-11-03 10:39:57 +0400

    minor fix

--- erc20.sol
+++ erc20.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
-
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
 
 contract SolicyCoinERC20 is ERC20 {
